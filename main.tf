c1bac8c6c1b5685ae3fcefb3bfb3068b9cd1f8ea
Andres Silverio Lopez Ospina <alopez29@summa-sci.com>
2022-07-26 16:49:19 -0500

    Asociando el SG a la instancia

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
   ami   = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
