c47ba4f74465ccd963506bc96987dd55251b0f31
Andres Silverio Lopez Ospina <alopez29@summa-sci.com>
2022-07-26 14:04:42 -0500

    Adicionando en el userdata el servicio web con el puerto 8080

--- main.tf
+++ main.tf
@@ -5,6 +5,14 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
   ami   = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
   
   tags = {
     Name = "terraform-example-ASLO2"
