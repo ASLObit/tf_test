5fdf82729a65a5f9ce4826c9337964a84a75a1c7
ASLObit <aslo.bit@gmail.com>
2022-07-26 18:10:35 -0500

    Adding port 8083

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8083
+}
 resource "aws_instance" "my_instance" {
   ami   = "ami-052efd3df9dad4825"
   instance_type = "t2.micro"
@@ -10,7 +15,7 @@ resource "aws_instance" "my_instance" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p ${var.server_port} &
               EOF
 
   user_data_replace_on_change = true
@@ -24,15 +29,9 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  variable "server_port" {
-    description = "The port the server will use for HTTP requests"
-    type = number
-  }
-  
+  }  
 }
