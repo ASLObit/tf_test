ee4fdfbf3932e2402da3242e85d0771f38a618e2
Andres Silverio Lopez Ospina <alopez29@summa-sci.com>
2022-07-26 17:53:47 -0500

    Adding variable of server port

--- main.tf
+++ main.tf
@@ -29,4 +29,10 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  variable "server_port" {
+    description = "The port the server will use for HTTP requests"
+    type = number
+  }
+  
 }
