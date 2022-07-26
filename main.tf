a5a9153aab4c1db1a4609d60e723bc9467a3fc70
ASLObit <93955852+ASLObit@users.noreply.github.com>
2022-07-26 12:48:08 -0500

    Primer commit
    Descripción del primer commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_instance" {
+  ami   = "ami-052efd3df9dad4825"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "terraform-example-ASLO"
+  }
+}
