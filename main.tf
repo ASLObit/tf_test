fb5c70b6999be58f5e072c13051d9c97ec12a343
ASLObit <aslo.bit@gmail.com>
2022-08-01 23:08:58 -0500

    Adiconando ASG y ALB

--- main.tf
+++ main.tf
@@ -4,24 +4,55 @@ provider "aws" {
 
 variable "server_port" {
   description = "The port the server will use for HTTP requests"
-  type = number
-  default = 8084
+  type        = number
+  default     = 8084
 }
-resource "aws_instance" "my_instance" {
-  ami   = "ami-052efd3df9dad4825"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+# resource "aws_instance" "my_instance" {
+#   ami                    = "ami-052efd3df9dad4825"
+#   instance_type          = "t2.micro"
+#   vpc_security_group_ids = [aws_security_group.instance.id]
+
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World ASLO" > index.html
+#               nohup busybox httpd -f -p ${var.server_port} &
+#               EOF
+
+#   user_data_replace_on_change = true
+
+#   tags = {
+#     Name = "terraform-example-ASLO"
+#   }
+# }
+
+resource "aws_launch_configuration" "ejemplo_instancia" {
+  image_id        = "ami-052efd3df9dad4825"
+  instance_type   = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World Andrés" > index.html
+              echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
+              
+  # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-  user_data_replace_on_change = true
-  
-  tags = {
-    Name = "terraform-example-Rey"
+resource "aws_autoscaling_group" "ejemplo_asg" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnets.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
 
@@ -33,5 +64,23 @@ resource "aws_security_group" "instance" {
     to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }  
+  }
+}
+
+resource "aws_lb" "example" {
+  name               = "terraform-asg-example"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+output "public_ip" {
+  value       = aws_instance.ejemplo_instancia.public_ip
+  description = "The public IP address of the web server"
 }
